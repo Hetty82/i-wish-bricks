faa09fe2f3eb1407c579cc9314402649ec358345
Hetty de Vries <hetty82@gmail.com>
2025-02-18 16:09:31 +0100

    Add html & css for header (static)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatBadgeModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
