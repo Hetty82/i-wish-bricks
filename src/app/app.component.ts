6f3eee783299b7bacec00c17e237398559b5c9d6
Hetty de Vries <hetty82@gmail.com>
2025-02-18 15:16:50 +0100

    Add and run prettier & eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DrawerComponent } from './drawer/drawer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DrawerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'I ❤️ bricks';
-}
+export class AppComponent {}
---
+++ src/app/drawer/drawer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drawer',
+  imports: [],
+  templateUrl: './drawer.component.html',
+  styleUrl: './drawer.component.scss',
+})
+export class DrawerComponent {}
